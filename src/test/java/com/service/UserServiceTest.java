9659babc66013c3f73962baab80427b8deab8799
WXuLiang <WXuLiang@users.noreply.github.com>
2018-01-26 13:20:50 +0800

    1

--- src/test/java/com/service/UserServiceTest.java
+++ src/test/java/com/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.service;
 
-import com.service.impl.UserService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +15,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-core.xml"})
 public class UserServiceTest {
+    /**
+     * 记录日志信息
+     */
+    private Logger logger = LogManager.getLogger(UserServiceTest.class);
 
     @Autowired
     private UserService userService;
 
     @Test
     public void findUserBeans() throws Exception {
+
+        logger.info("log日志记录------->>  打印输出所有用户信息");
+
         userService.findUserBeans();
+
     }
 
 }
